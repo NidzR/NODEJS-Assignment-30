ffa7d590fd3a0b1a742523e42c47ca8802061d08
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-01-14 03:58:28 +0500

    NODEJ Assignment Q30
    30.  Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
    • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
    • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.

---
+++ Q30.js
@@ -0,0 +1,12 @@
+"use strict";
+//30.Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
+// • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+let employee = ["admin", "Rizwan Akhtar", "Ahsen Rizwan", "Nida Rizwan", "Rumaisa Rizwan"];
+for (let i = 0; i < employee.length; i++) {
+    if (employee[i] == "admin") {
+        console.log("Hello admin, would you like to see a status report?");
+    }
+    else {
+        console.log(`Hello ${employee[i]}`);
+    }
+}
